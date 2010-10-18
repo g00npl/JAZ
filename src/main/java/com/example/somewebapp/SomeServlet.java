f3a41511b7f4c004a8a4aad67a9e69338cf6e3ec
Lukasz Czerepuk <s7267@gda001-13.pjwstk.edu.pl>
2010-10-18 12:12:13 +0200

    18101001

---
+++ src/main/java/com/example/somewebapp/SomeServlet.java
@@ -0,0 +1,29 @@
+package com.example.somewebapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/message")
+public class SomeServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();		
+		out.print("<html><body><h1>Hello World! from servlet (ver. 3.0)</h1></body></html>");
+		out.close();
+		
+	}
+
+}
